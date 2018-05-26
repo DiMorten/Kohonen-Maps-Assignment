073130ef1b1cff281ccdb11e8a2f12116c48ec6c
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2018-05-26 11:11:09 -0300

    Rough -finetune phases done

--- src/main.m
+++ src/main.m
@@ -11,17 +11,25 @@ conf=struct();
 %conf.mode='auto';
 % BEST SO FAR
 
-conf.mode='auto';
-conf.mode='auto';
+%conf.mode='auto';
+conf.mode='manual';
 
 conf.msize=[12 6];
 conf.lattice='hexa';
-conf.radius=[6 1];
+conf.radius=[1 1];
 conf.trainlen=100;
 conf.alpha_ini=0.5;
+conf.rough.alpha_ini=0.5;
+conf.finet.alpha_ini=0.05;
+conf.rough.trainlen=2;
+conf.finet.trainlen=6;
+conf.shape='sheet';
 % SEARCH BEGINS
 conf.lattice='hexa';
+%conf.lattice='rect';
 
+conf.dlen=size(sD.data,1);
+conf.algorithm='batch';
 %%
 
 sD = som_normalize(sD,'range');
@@ -29,25 +37,36 @@ if isequal(conf.mode,'auto')
     sM = som_make(sD);  %Batch training method
 else
     %sM = som_randinit(sD,'msize',conf.msize);
-    sM = som_lininit(sD,'msize',conf.msize);
+    sM = som_lininit(sD,'msize',conf.msize,'lattice',conf.lattice,'shape',conf.shape);
     
+    %conf.algorithm='batch';
+    %sTrain = som_train_struct(sM,'dlen',conf.dlen,'algorithm',conf.algorithm,'phase','rough');
+    %sTrain.alpha_ini=conf.alpha_ini;
+    %sM.topol.lattice='rect';
     %sM.
-    %sM = som_seqtrain(sM,sD,'radius',conf.radius, ...
-    sM = som_batchtrain(sM,sD,'radius',conf.radius, ...
-        'msize',conf.msize,'trainlen',conf.trainlen,...
+    %sM = som_batchtrain(sM,sD,'radius',conf.radius, ...
+    
+    % ROUGH TRAINING PHASE 
+    sM = som_seqtrain(sM,sD,'radius',conf.radius, ...
+        'msize',conf.msize,'trainlen',conf.rough.trainlen,...
         'trainlen_type','epochs',...
         'lattice',conf.lattice, ...
-        'alpha_ini', conf.alpha_ini, ...
+        'shape',conf.shape, ...
+        'alpha_ini', conf.rough.alpha_ini, ...
         'tracking',0);
     [e1,e2,e3]=som_quality(sM,sD);
     fprintf('Final quantization error: %5.3f\n',e1)
     fprintf('Final topographic error:  %5.3f\n',e2)
     fprintf('Final combined error:  %5.3f\n',e3)  
-    sM = som_batchtrain(sM,sD,'radius',conf.radius, ...
-        'msize',conf.msize,'trainlen',conf.trainlen,...
+
+    %sM = som_batchtrain(sM,sD,'radius',conf.radius, ...
+    % FINETUNING TRAINING PHASE
+    sM = som_seqtrain(sM,sD,'radius',conf.radius, ...
+        'msize',conf.msize,'trainlen',conf.finet.trainlen,...
         'trainlen_type','epochs',...
         'lattice',conf.lattice, ...
-        'alpha_ini', 0.05, ...
+        'shape',conf.shape, ...
+        'alpha_ini', conf.finet.alpha_ini, ...
         'tracking',0);
     [e1,e2,e3]=som_quality(sM,sD);
     fprintf('Final quantization error: %5.3f\n',e1)
