53d8a81eeaef9d88b67c5b7ad4c1b6c6373b79c6
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2018-05-27 00:44:27 -0300

    Tested topology. Before others

--- src/main.m
+++ src/main.m
@@ -2,7 +2,8 @@
 
 close all
 clc
-
+addpath(genpath('/home/jorg/Documents/Master/scnd_semester/neural_nets/homework2/repo/src/plotSpread'))
+addpath(genpath('SOM-Toolbox-master'));
 in_filename='../data/new-thyroid.data';
 %sD = som_read_data(in_filename);
 sD = som_read_data(in_filename);
@@ -24,6 +25,8 @@ conf.finet.alpha_ini=0.05;
 conf.rough.trainlen=2;
 conf.finet.trainlen=6;
 conf.shape='sheet';
+%conf.shape='cyl';
+
 % SEARCH BEGINS
 conf.lattice='hexa';
 %conf.lattice='rect';
@@ -32,8 +35,144 @@ conf.dlen=size(sD.data,1);
 conf.algorithm='batch';
 
 conf.plot=0;
-%%
+conf.manytimes.times=100;
+conf.manytimes.e=struct();
+conf.manytimes.e.v=zeros(conf.manytimes.times,3);
+%
+conf.exp_id=1;
+result=struct();
 
+if conf.exp_id==1
+    %conf.exp_data=[1]
+    %conf.exp_len=length(conf.exp_data);
+    conf.exp_len=6;
+    result.v=zeros(3,conf.exp_len);
+
+end
+%conf.manytimes.e.e1=struct();conf.manytimes.e.e2=struct();conf.manytimes.e.e3=struct();
+%conf.manytimes.e.e1.v=zeros(1,conf.manytimes.times);conf.manytimes.e.e2.v=zeros(1,conf.manytimes.times);conf.manytimes.e.e3.v=zeros(1,conf.manytimes.times);
+
+%conf.manytimes.e.e1.mean=mean(conf.manytimes.e.e1.v);
+%conf.manytimes.e.e2.mean=mean(conf.manytimes.e.e2.v);
+%conf.manytimes.e.e3.mean=mean(conf.manytimes.e.e3.v);
+count=0;
+for i=1:2
+    for k=1:3
+        count=count+1;
+        if i==1, conf.lattice='hexa'; else, conf.lattice='rect'; end
+        
+        if k==1, conf.shape='sheet'; 
+        elseif k==2, conf.shape='cyl'; 
+        elseif k==3, conf.shape='toroid'; 
+        end
+         
+        %hexa:sheet,cyl,toroid,rect:sheet,cyl,toroid
+        
+        [sD,sM,conf] = som_train_many(sD,conf);
+        results.v(:,count)=conf.manytimes.e.min;
+        results.e1(i,k)=conf.manytimes.e.min(1,1);
+        results.e2(i,k)=conf.manytimes.e.min(1,2);
+        results.e3(i,k)=conf.manytimes.e.min(1,3);
+        
+        results.lattice(:,count)=i;
+        results.shape(:,count)=k;
+        
+    end
+end
+% figure,subplot(1,3,1),mesh(results.e1);
+% subplot(1,3,2),mesh(results.e2);
+% subplot(1,3,3),mesh(results.e3);
+%%
+catsx={'hexa','rect'};
+catsy={'sheet','cyl','toroid'};
+z=results.e3';
+mesh3d(catsx,catsy,z,1);hold on;
+xlabel('Lattice')
+ylabel('Shape')
+zlabel('Combined Error')
+%%
+figure,scatter3(results.lattice,results.shape,results.v(1,:))
+
+%%
+function [sD,sM,result] = som_train_topology(sM,sD,conf,result)
+
+
+%conf.manytimes.e.e1=struct();conf.manytimes.e.e2=struct();conf.manytimes.e.e3=struct();
+%conf.manytimes.e.e1.v=zeros(1,conf.manytimes.times);conf.manytimes.e.e2.v=zeros(1,conf.manytimes.times);conf.manytimes.e.e3.v=zeros(1,conf.manytimes.times);
+
+%conf.manytimes.e.e1.mean=mean(conf.manytimes.e.e1.v);
+%conf.manytimes.e.e2.mean=mean(conf.manytimes.e.e2.v);
+%conf.manytimes.e.e3.mean=mean(conf.manytimes.e.e3.v);
+count=0;
+for i=1:2
+    for k=1:3
+        count=count+1;
+        if i==1, conf.lattice='hexa'; else, conf.lattice='rect'; end
+        
+        if k==1, conf.shape='sheet'; 
+        elseif k==2, conf.shape='cyl'; 
+        elseif k==3, conf.shape='toroid'; 
+        end
+         
+        %hexa:sheet,cyl,toroid,rect:sheet,cyl,toroid
+        
+        [sD,sM,conf] = som_train_many(sD,conf);
+        results.v(:,count)=conf.manytimes.e.min;
+        results.e1(i,k)=conf.manytimes.e.min(1,1);
+        results.e2(i,k)=conf.manytimes.e.min(1,2);
+        results.e3(i,k)=conf.manytimes.e.min(1,3);
+        
+        results.lattice(:,count)=i;
+        results.shape(:,count)=k;
+        
+    end
+end
+% figure,subplot(1,3,1),mesh(results.e1);
+% subplot(1,3,2),mesh(results.e2);
+% subplot(1,3,3),mesh(results.e3);
+%%
+catsx={'hexa','rect'};
+catsy={'sheet','cyl','toroid'};
+z=results.e3';
+mesh3d(catsx,catsy,z,1);hold on;
+xlabel('Lattice')
+ylabel('Shape')
+zlabel('Combined Error')
+end
+%%
+function mesh3d(catsx,catsy,z,categorical_flag)
+if categorical_flag==1
+    c = categorical(catsx,'Ordinal',true);
+    xcataxis = reordercats(c,catsx);
+    c = categorical(catsy,'Ordinal',true);
+    ycataxis = reordercats(c,catsy);
+    [X,Y]=meshgrid(xcataxis,ycataxis);
+    figure,mesh(X,Y,z);    
+end
+end
+%%
+function [sD,sM,conf] = som_train_many(sD,conf)
+
+for i=1:1:conf.manytimes.times
+    [sD,sM,e]=som_train_once(sD,conf);
+    conf.manytimes.e.v(i,:)=e';
+   
+end
+for k=1:3
+    conf.manytimes.e.mean(1,k)=mean(conf.manytimes.e.v(:,k));
+    conf.manytimes.e.median(1,k)=median(conf.manytimes.e.v(:,k));
+    conf.manytimes.e.std(1,k)=std(conf.manytimes.e.v(:,k));
+    conf.manytimes.e.min(1,k)=min(conf.manytimes.e.v(:,k));
+    
+end
+if conf.plot==3
+    plotSpread(conf.manytimes.e.v(:,1))
+    plotSpread(conf.manytimes.e.v(:,2))
+    plotSpread(conf.manytimes.e.v(:,3))
+end
+end
+%%
+function [sD,sM,e] = som_train_once(sD,conf)
 sD = som_normalize(sD,'range');
 if isequal(conf.mode,'auto')
     sM = som_make(sD);  %Batch training method
@@ -57,9 +196,9 @@ else
         'alpha_ini', conf.rough.alpha_ini, ...
         'tracking',0);
     [e1,e2,e3]=som_quality(sM,sD);
-    fprintf('Final quantization error: %5.3f\n',e1)
-    fprintf('Final topographic error:  %5.3f\n',e2)
-    fprintf('Final combined error:  %5.3f\n',e3)  
+    %fprintf('Final quantization error: %5.3f\n',e1)
+    %fprintf('Final topographic error:  %5.3f\n',e2)
+    %fprintf('Final combined error:  %5.3f\n',e3)  
 
     %sM = som_batchtrain(sM,sD,'radius',conf.radius, ...
     % FINETUNING TRAINING PHASE
@@ -70,10 +209,10 @@ else
         'shape',conf.shape, ...
         'alpha_ini', conf.finet.alpha_ini, ...
         'tracking',0);
-    [e1,e2,e3]=som_quality(sM,sD);
-    fprintf('Final quantization error: %5.3f\n',e1)
-    fprintf('Final topographic error:  %5.3f\n',e2)
-    fprintf('Final combined error:  %5.3f\n',e3)  
+    [e(1,1),e(1,2),e(1,3)]=som_quality(sM,sD);
+    %fprintf('Final quantization error: %5.3f\n',e(1,1))
+    %fprintf('Final topographic error:  %5.3f\n',e(1,2))
+    %fprintf('Final combined error:  %5.3f\n',e(1,3))  
 end
 sM = som_autolabel(sM,sD,'vote'); %The best matching unit of each sample is found. The label is given to the map unit.
 
@@ -109,4 +248,5 @@ if f.plot==1
     xlabel('Feature 1: resina-T3')
     ylabel('Feature 2: Tiroxina total')
     zlabel('Feature 3: TSH')
+end
 end
